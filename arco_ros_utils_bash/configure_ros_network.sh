1590099e3d97a3b43a69ae79eb347d19210c81a2
marco-tranzatto <marcot@ethz.ch>
2020-02-15 10:30:00 -0800

    configure ros master wiht ethernet port

--- arco_ros_utils_bash/configure_ros_network.sh
+++ arco_ros_utils_bash/configure_ros_network.sh
@@ -2,10 +2,10 @@
 
 #Configure ROS Network with a master running on a remote computer
 echo "Usage : source $0 <MASTER_IP>"
-wifi_interface_name="wlp3s0"
-#get WIFI IP address of this machine
-wifiip=$(ip addr | awk '/inet/ && /'${wifi_interface_name}'/{sub(/\/.*$/,"",$2); print $2}')
-export ROS_IP=$wifiip
+network_interface_name="enp0s31f6"
+#get IP address of this network interface
+machine_ip=$(ip addr | awk '/inet/ && /'${network_interface_name}'/{sub(/\/.*$/,"",$2); print $2}')
+export ROS_IP=$machine_ip
 export ROS_MASTER_URI=http://"$1":11311
 echo "ROS_IP="$ROS_IP
 echo "ROS_MASTER_URI"=$ROS_MASTER_URI
